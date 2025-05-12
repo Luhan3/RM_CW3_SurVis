b935965db83ba823ce0c74e30d4bbd4c314ab567
Luhan3 <luhanli_@outlook.com>
2025-05-12 14:57:52 +0100

    Add files via upload

---
+++ survis_0/src/data/tag_categories.js
@@ -0,0 +1,14 @@
+define({
+    tagCategories: {
+        "type": {"description": "Type of publication (e.g., journal, conference)"},    
+        "evaluation": {
+            "description": "Methods used to validate research findings, including empirical studies, comparative analyses, and expert reviews."
+        },  
+        "domain": {
+            "description": "Research domain (e.g., healthcare, economics)"
+        },
+        "method": {
+            "description": "Methodology used in the study"
+        }
+    }
+});
