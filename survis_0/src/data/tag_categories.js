c7eb5f164ea902a154c582ba2b4e9fbd0112dbdd
Luhan3 <luhanli_@outlook.com>
2025-05-15 20:25:56 +0100

    tag_categories.js

--- survis_0/src/data/tag_categories.js
+++ survis_0/src/data/tag_categories.js
@@ -1,6 +1,7 @@
 define({
     tagCategories: {
         "type": {"description": "Type of publication (e.g., journal, conference)"},    
+        "category": {"description": "Category focus."}, 
         "evaluation": {
             "description": "Methods used to validate research findings, including empirical studies, comparative analyses, and expert reviews."
         },  
